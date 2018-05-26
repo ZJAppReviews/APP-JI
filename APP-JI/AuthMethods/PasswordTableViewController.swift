bdfd2d65344a26461efa4d2cf284ebb920d511d3
huangdaxianer <huang0623@gmail.com>
2018-05-27 00:16:18 +0800

    觉得把它放在TableView的Cell里好奇怪，打算换个方式拖拖看

--- APP-JI/AuthMethods/PasswordTableViewController.swift
+++ APP-JI/AuthMethods/PasswordTableViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class PasswordTableViewController: UITableViewController {
+    
+    @IBOutlet var passwordView:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        passwordView.addConstraint(<#T##constraint: NSLayoutConstraint##NSLayoutConstraint#>)
     }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        scrollView.isScrollEnabled = false
+    }
+    
+}
+
+class PasswordView: UIView {
+    
+
+    @IBOutlet var firstLab: UILabel!
+    @IBOutlet var secondLab: UILabel!
+    @IBOutlet var thirdLab: UILabel!
+    @IBOutlet var fourthLab: UILabel!
+    
+
+    
+    
+    
 }
